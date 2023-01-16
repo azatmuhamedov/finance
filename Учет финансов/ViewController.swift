59d0e8c895a5ca660c1f94faf35bf939542c2d1d
azat <azotmuhamed@gmail.com>
2023-01-16 20:49:32 +0300

    Initial Commit

---
+++ Учет финансов/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Учет финансов
+//
+//  Created by Азат Мухамедов on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
