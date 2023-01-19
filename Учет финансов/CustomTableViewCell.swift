40dd1c9079bc54928f776d45f7cfbb7cc397673e
azat <azotmuhamed@gmail.com>
2023-01-19 20:56:11 +0300

    add outlet

---
+++ Учет финансов/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  Учет финансов
+//
+//  Created by Азат Мухамедов on 19.01.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var recordImage: UIImageView!
+    
+    @IBOutlet weak var recordCost: UILabel!
+    @IBOutlet weak var recordCategory: UILabel!
+}
--- Учет финансов/ViewController.swift
+++ Учет финансов/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    var stillTyping = false
+    @IBOutlet var numberFromKeyboard: [UIButton]! {
+        didSet {                                    //наблюдатель для свойств
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+    
+    var categoryName = ""
+    var displayValue = ""
+    
+    @IBOutlet weak var tableVIew: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        
+        if stillTyping {
+            
+            if displayLabel.text!.count < 15 {  //чтобы более 15 цифр не вводилось
+                displayLabel.text = displayLabel.text! + number
+            }
+        } else {
+            displayLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+    @IBAction func categoryPressed(_ sender: UIButton) {
+        categoryName = sender.currentTitle!
+        displayValue = displayLabel.text!
+        displayLabel.text = "0"
+        stillTyping = false
+        
+        
+    }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    1
+}
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
 }
 
